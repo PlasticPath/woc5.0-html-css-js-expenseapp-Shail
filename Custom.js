f34ea7a4f54824594aea3d7a89fc9cea106f8331
h202201297 <122286033+h202201297@users.noreply.github.com>
2023-02-05 00:43:17 +0530

    Last HTML

--- Custom.js
+++ Custom.js
@@ -1,22 +1,3 @@
-// function AddValue()
-// {
-// 	//debugger;
-// 	var A1 = document.getElementById("lbl1").innerHTML;
-// 	alert(A1);
-// 	var A2=parseInt(A1);
-	
-// 	var B1 = document.getElementById("addvalue").value
-// 	alert(B1);
-// 	var B2=parseInt(B1);
-	
-// 	document.getElementById("lbl1").innerHTML = B2+A2;
-// 	alert(B2+A2);
-// }
-
-function New()
-{
-	document.getElementById("lbl1").innerHTML="Hi";
-}
 function AddValue()
 {
 	var a=document.getElementById("e1").value;
@@ -26,4 +7,16 @@ function AddValue()
 	var a2=parseInt(a);
 	var b2=parseInt(b);
 	document.getElementById("heading").innerHTML=a2+b2;
+}
+
+
+function Reload()
+{
+	location.reload();
+}
+
+function New()
+{
+	location.reload();
+	
 }
